15f899ea059e0684cdc538adfda141071cd8d772
Drashti Modi <drashtimodi21@gmail.com>
2024-01-31 12:03:01 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -16,7 +16,7 @@ createBtn.addEventListener("click", () => {
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
-    img.src = "images/Capture.png";
+    img.src = "images/Capture.PNG";
 
     let s1 = document.createElement("span");
     let s2 = document.createElement("span");
